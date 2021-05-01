70ccb707597b672d48d61484fe0d93cf090fc9b5
Ashish Kumar Satyam <ashishsatyam.kmr@gmail.com>
2021-05-01 15:00:47 +0530

    minor change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./index')
-
-app.listen(3000, (err) => {
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
     if (err) throw err
     console.log('Server running in http://127.0.0.1:3000')
 })
